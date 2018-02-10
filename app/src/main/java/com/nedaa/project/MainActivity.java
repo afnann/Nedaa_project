fb3991da48408651b4dec39a62f045fa17b73965
afnann <fefe003.g.m@gmail.com>
2018-02-10 23:24:49 +0300

    comment

---
+++ app/src/main/java/com/nedaa/project/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nedaa.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    EditText name=(EditText) findViewById(R.id.nameEditText);
+}
